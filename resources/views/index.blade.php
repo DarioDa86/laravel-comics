7451a31b8afc286f919735e298feb2c091ff7ff7
DarioDa86 <dario.dagnelli@gmail.com>
2021-11-07 17:48:57 +0100

    terminata anche la banda azzurra delle icone

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.base')
 
 @section('pageContent')
-    <section class="container ">
+    <section class="container">
 
         <ul class="comics">
             <div class="tag-series" >
@@ -19,5 +19,16 @@
         <div class="load-btn">
             <a href="#">LOAD MORE</a>
         </div>
+    </section>
+    <section class="icons" >
+        <ul class="row-icons container">
+            
+            <li><a href="#"><img src="{{asset("images/buy-comics-digital-comics.png")}}" alt="Logo DC"> DIGITAL COMICS</a></li>
+            <li><a href="#"><img src="{{asset("images/buy-comics-merchandise.png")}}" alt="Logo DC"></a><a href="#">DC MERCHANDISE</a></li>
+            <li><a href="#"><img src="{{asset("images/buy-comics-subscriptions.png")}}" alt="Logo DC"></a><a href="#">SUBSCRIPTION</a></li>
+            <li><a href="#"><img src="{{asset("images/buy-comics-shop-locator.png")}}" alt="Logo DC"></a><a href="#">COMIC SHOP LOCATOR</a></li>
+            <li><a href="#"><img src="{{asset("images/buy-dc-power-visa.svg")}}" alt="Logo DC"></a><a href="#">DC POWER VISA</a></li>
+            
+        </ul>
     </section>
 @endsection
